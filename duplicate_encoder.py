88c0c955429b8804d5595aca9876e8cf999b57f7
user171717x <user171717user@protonmail.com>
2022-01-18 13:18:40 +0200

    Codewars first kata

---
+++ duplicate_encoder.py
@@ -0,0 +1,8 @@
+def duplicate_encode(word):
+    word_array = ['(' if word.lower().count(symbol) == 1 else ')' for symbol in word]
+    return "".join(word_array)
+
+
+# print(duplicate_encode("Success"))
+# print(duplicate_encode("(( @"))
+print(duplicate_encode('))(()())())'))
